342dccbd663622c22edcd63c2cf511c7999b22eb
Ilia Goncharenko <danteoutoftime@gmail.com>
2020-02-04 18:41:32 -0500

    implemented getSingleChat and getUserChats

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -1,20 +1,33 @@
 // External Dependencies
 const boom = require('boom');
 const ChatMessageModel = require('../models/ChatMessage');
+const User = require('../models/User');
 
 // Get Data Models
 const Chat = require('../models/Chat');
 
-// TODO: fix this method
-// Get chat based on trip id
+// Get chat based on chat id
 exports.getSingleChat = async (req, reply) => {
     try{
-        const id = req.body.chatId;
-        console.log(id, "This is id");
-        console.log(req.body, "This is my body");
-        // const chat = Chat.find({trip_id:id});
-        // return chat;
+        const id = req.params.id;
+        const chat = await Chat.findById(id);
+        // return messages related to chat
+        const messages = await Chat.findById(id).populate('chatMessages');
+        return messages;
     }catch (err) {
+        throw boom.boomify(err);
+    }
+};
+
+// Get all user chats
+exports.getUserChats = async (req, reply) => {
+    try {
+        const id = req.params.id;
+        console.log(id, 'This is id');
+        // const user = await User.findById(id);
+        const chats = await User.findById(id).populate('chats');
+        return chats;
+    }catch(err) {
         throw boom.boomify(err);
     }
 };
